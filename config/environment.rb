d0194c7f2dd834014f6189f65a6818016a5a0399
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-18 19:22:00 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -14,17 +14,5 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite" 
   )
-  
-# sql = <<-SQL 
-#   CREATE TABLE IF NOT EXISTS artists (
-#   id INTEGER PRIMARY KEY,
-#   name TEXT,
-#   genre TEXT,
-#   age INTEGER,
-#   hometown TEXT
-#   )
-# SQL
-
-# AcriveRecord::Base.connection.execute(sql)
 
 require_relative "../artist.rb"
